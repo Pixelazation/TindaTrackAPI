1caf0bd7f6c937b78b81000129e5f01392a160ff
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-06-12 14:10:12 +0800

    Add project files.

---
+++ TindaTrackAPI/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TindaTrackAPI.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string ItemCode { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required ICollection<Sale> Sales { get; set; }
+    }
+
+}
---
+++ TindaTrackAPI/Models/Sale.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TindaTrackAPI.Models
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int SalesmanId { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }  // stored in pcs
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        public decimal TotalAmount => Quantity * UnitPrice;
+
+        [Required]
+        public string Barangay { get; set; } = string.Empty;
+
+        [Required]
+        public string Municipality { get; set; } = string.Empty;
+
+        // Navigation properties
+        [Required]
+        public Salesman Salesman { get; set; } = null!;
+
+        [Required]
+        public Item Item { get; set; } = null!;
+    }
+}
