1a11e277f611919ce2e84004bc97852cbdc66601
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-06-14 17:13:33 +0800

    Added endpoints for location data

---
+++ TindaTrackAPI/Controllers/BarangaysController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TindaTrackAPI.DTOs.Barangay;
+using TindaTrackAPI.Models;
+
+namespace TindaTrackAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarangaysController : ControllerBase
+    {
+        private readonly TindaTrackContext _context;
+
+        public BarangaysController(TindaTrackContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Barangays
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BarangayDto>>> GetBarangays()
+        {
+            var barangays = await _context.Barangays
+            .Select(barangay => new BarangayDto
+            {
+                Id = barangay.Id,
+                Name = barangay.Name,
+            })
+            .ToListAsync();
+
+            return Ok(barangays);
+        }
+    }
+}
---
+++ TindaTrackAPI/Controllers/MunicipalitiesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TindaTrackAPI.DTOs.Municipality;
+using TindaTrackAPI.Models;
+
+namespace TindaTrackAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MunicipalitiesController : ControllerBase
+    {
+        private readonly TindaTrackContext _context;
+
+        public MunicipalitiesController(TindaTrackContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Municipalities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MunicipalityDto>>> GetMunicipalities()
+        {
+            var municipalities = await _context.Municipalities
+            .Select(municipality => new MunicipalityDto
+            {
+                Id = municipality.Id,
+                Name = municipality.Name,
+            })
+            .ToListAsync();
+
+            return Ok(municipalities);
+        }
+    }
+}
